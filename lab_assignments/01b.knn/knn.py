a61d239ffae254fe5967db2e3352911bcea29e92
Marco Zullich <marco.zullich@gmail.com>
2023-11-29 18:56:03 +0100

    knn refactoring added type hints

--- lab_assignments/01b.knn/knn.py
+++ lab_assignments/01b.knn/knn.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 class KNearestNeighbors:
-    def __init__(self, num_neighbors, data=None):
+    def __init__(self, num_neighbors:int, data:np.array=None) -> None:
         self.num_neighbors = num_neighbors
         self.data = data
-        
+    
     
 
         
