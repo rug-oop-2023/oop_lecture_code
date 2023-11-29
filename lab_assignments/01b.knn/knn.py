0e81171cacdb0a3c7d45b9a71fdce9ab63c1cdcc
Marco Zullich <marco.zullich@gmail.com>
2023-11-29 18:48:25 +0100

    Init test data

--- lab_assignments/01b.knn/knn.py
+++ lab_assignments/01b.knn/knn.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 class KNearestNeighbors:
-    def __init__(self, num_neighbors):
+    def __init__(self, num_neighbors, data=None):
         self.num_neighbors = num_neighbors
+        self.data = data
+        
+    
+
         
--- tests/knn_test.py
+++ tests/knn_test.py
@@ -1,10 +1,21 @@
 import unittest
+
+import numpy as np
+
 import sys
 sys.path.append('lab_assignments/01b.knn')
 import knn
 
 class KNearestNeighborsTest(unittest.TestCase):
-    def test_init(self):
+    def test_init_k(self):
         num_neighbors = 3
         knn_model = knn.KNearestNeighbors(num_neighbors)
         self.assertEqual(knn_model.num_neighbors, num_neighbors)
+    
+    def test_init_data(self):
+        num_neighbors = 3
+        sample_data = np.array([[1,2,3],[4,5,6]])
+        knn_model = knn.KNearestNeighbors(num_neighbors)
+        self.assertEqual(knn_model.data, None)
+        knn_model = knn.KNearestNeighbors(num_neighbors, sample_data)
+        np.testing.assert_array_equal(knn_model.data, sample_data)
