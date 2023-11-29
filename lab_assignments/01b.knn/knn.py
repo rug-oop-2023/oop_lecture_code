e21db4f2dbdc1ae141e0e0beb6baf739f40ecd80
Marco Zullich <marco.zullich@gmail.com>
2023-11-29 19:02:10 +0100

    Refactoring knn

--- lab_assignments/01b.knn/knn.py
+++ lab_assignments/01b.knn/knn.py
@@ -1,11 +1,12 @@
 import numpy as np
+from typing import List
 
 class KNearestNeighbors:
     def __init__(self, num_neighbors:int, data:np.array=None) -> None:
         self.num_neighbors = num_neighbors
         self.data = data
     
-    def _return_neighbors(self, data_point:np.array) -> int:
+    def _return_neighbors(self, data_point:np.array) -> List[int]:
         """
         Return the index of the closest data point to the given data point.
         """
